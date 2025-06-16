4ce02b752b21e5a031903ad777b8c74818e4fc44
Om Gupta <ogupta4242@gmail.com>
2025-06-16 10:17:50 -0700

    feat(project): init

---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+        google()
+    }
+}
+
+rootProject.name = "multipage"
+
+include(":site")
+include("server")
